18277b17b8627cd96b76ceaad12041c8c57ef922
Senimantas <neimetas@gmail.com>
2016-04-18 14:42:43 +0200

    First commit
    Uploading the Unity project with these short code examples:
    
    Singleton
    ScriptableObjects
    Events

---
+++ Assets/Events/Scripts/EventManagerSingleton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManagerSingleton
+{
+	private static EventManagerSingleton _instance = null;
+
+	// Get the instance of the EventManagerSingleton
+	public static EventManagerSingleton instance
+	{
+		// This ensures that the instance can only be gotten, never set from the outside the class
+		get
+		{
+			// If the instance does not yet exist, then create one
+			if (_instance == null)
+				_instance = new EventManagerSingleton();
+			return _instance;
+		}
+	}
+
+	// OnClick event handle
+	public delegate void ClickAction();
+	public event ClickAction OnClicked;
+
+	// Private constructor to prevent initialization from outside the class
+	private EventManagerSingleton () {}
+}
---
+++ Assets/Singleton/Scripts/Singleton/Manager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+
+    public static Manager instance = null;
+
+    public int levelNumber = 3;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+	}
+}
